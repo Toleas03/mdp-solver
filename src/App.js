c877e69e2a57b562ef77ec0afb3d1ce34465275c
3marwanmoraly3 <152908509+3marwanmoraly3@users.noreply.github.com>
2024-02-09 19:24:34 +0300

    Added tailwindcss support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Body />
     </div>
--- src/components/Logic.js
+++ src/components/Logic.js
@@ -13,3 +13,38 @@ function FunctionTwo() {
   }
 
 export {FunctionOne, FunctionTwo}
+
+/*
+const input = {
+  discount: 1,
+  iterations: 5,
+  states: [
+      {
+          name: "State 1",
+          connections: [
+              {
+                  name: "Action 1",
+                  destinations: [{
+                      name: "State 1",
+                      prob: 1,
+                      reward: 5
+                  },]
+              },
+          ]
+      },
+  ]
+}
+
+const blyat = [
+  {
+      name: "State 1",
+      iterations: [
+          {
+              name: "V0",
+              value: 69,
+              formula: "68 + 1"
+          },
+      ]
+  },
+]
+*/
